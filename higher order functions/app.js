ad2e3d49a2d10d14c361cf7899f91417d8ef43dd
alizayJ <116749097+alizayJ@users.noreply.github.com>
2024-05-10 00:23:14 +0500

    Add files via upload

---
+++ higher order functions/app.js
@@ -0,0 +1,16 @@
+function print_name(name){
+    return name;
+}
+function last_name(last){
+    return last;
+}
+function fullname(function1, function2){
+    console.log(`your name is ${function1} ${function2}`)
+}
+fullname(print_name("Aliza"), last_name("Kashif"));
+ 
+
+function color(val){
+    return val;
+}
+color(fullname(print_name("aliza"), last_name("kashif")));
